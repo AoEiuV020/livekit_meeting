28cc8bcc0c3b1099d2c241722e557092dc68bd86
AoEiuV020 <aoeiuv020@gmail.com>
2024-10-17 23:42:33 +0800

    创建module
    先简单依赖主plugin库，

---
+++ script/module_create.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+. "$(dirname $0)/env.sh"
+
+name=$1
+# name 传入了包名就在packages创建包，否则创建修改根项目，
+if [ -z "$name" ] || [ "$name" = "." ]; then
+    cd "$ROOT"
+    rm -f pubspec.yaml README.md
+    mv .gitignore .gitignore.bak
+    flutter create --template=module .
+    dart pub add dev:melos
+    cat .gitignore.bak >>.gitignore
+    rm -f .gitignore.bak
+else
+    # 检查并创建目录
+    if [ ! -d "$packages_dir" ]; then
+        echo "Directory $packages_dir does not exist. Creating..."
+        mkdir -p "$packages_dir"
+    fi
+    cd "$packages_dir"
+    flutter create --template=module "$name"
+    cd "$name"
+fi
+echo 'include: package:flutter_lints/flutter.yaml' >analysis_options.yaml
+cat "$script_dir"/analyzer_custom.yaml >>analysis_options.yaml
+"$script_dir"/prepare.sh
