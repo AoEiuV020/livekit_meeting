f906aed6eaf0a8436b913720ae019ff8a2e102dd
AoEiuV020 <aoeiuv020@gmail.com>
2024-10-18 16:35:13 +0800

    create module example android app

---
+++ packages/meeting_module/example/android/app/src/main/java/com/aoeiuv020/meetingmoduleexample/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.aoeiuv020.meetingmoduleexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aoeiuv020.meetingmoduleexample.ui.theme.MeetingModuleExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MeetingModuleExampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MeetingModuleExampleTheme {
+        Greeting("Android")
+    }
+}
