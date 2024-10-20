188fd7808d4f76e10116b4c66eac5836fa1909be
AoEiuV020 <aoeiuv020@gmail.com>
2024-10-20 21:11:44 +0800

    添加flutter activity
    默认配置直接点击打开flutter页面，

--- packages/meeting_module/example/android/app/src/main/java/com/aoeiuv020/meetingmoduleexample/MainActivity.kt
+++ packages/meeting_module/example/android/app/src/main/java/com/aoeiuv020/meetingmoduleexample/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.aoeiuv020.meetingmoduleexample.ui.theme.MeetingModuleExampleTheme
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,9 +35,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.clickable {
+            context.startActivity(
+                FlutterActivity
+                    .withNewEngine()
+                    .build(context)
+            )
+        }
     )
 }
 
--- packages/meeting_module/example/android/settings.gradle.kts
+++ packages/meeting_module/example/android/settings.gradle.kts
@@ -19,6 +19,7 @@ dependencyResolutionManagement {
         maven("$storageUrl/download.flutter.io")
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
