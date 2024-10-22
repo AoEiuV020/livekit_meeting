47f587c8dfb7644de288ba52de42923461d8982c
AoEiuV020 <aoeiuv020@gmail.com>
2024-10-22 21:19:34 +0800

    实体使用"Dart Data Class Generator"生成json相关转换方法

--- lib/api/bean/server_token.dart
+++ lib/api/bean/server_token.dart
@@ -1,12 +1,27 @@
+import 'dart:convert';
+
 class ServerToken {
   String serverUrl;
   String token;
 
   ServerToken({required this.serverUrl, required this.token});
 
-  // 从 Map 转为 对象
-  factory ServerToken.fromJson(Map<String, dynamic> json) => ServerToken(
-    serverUrl: json['serverUrl'] as String,
-    token: json['token'] as String,
-  );
+  Map<String, dynamic> toMap() {
+    return {
+      'serverUrl': serverUrl,
+      'token': token,
+    };
+  }
+
+  factory ServerToken.fromMap(Map<String, dynamic> map) {
+    return ServerToken(
+      serverUrl: map['serverUrl'] ?? '',
+      token: map['token'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ServerToken.fromJson(String source) =>
+      ServerToken.fromMap(json.decode(source));
 }
