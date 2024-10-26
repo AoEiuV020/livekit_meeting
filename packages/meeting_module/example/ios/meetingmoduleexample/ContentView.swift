2f364e27e8a911e39dd4bd0bc1d0b3cf20149e0a
AoEiuV020 <aoeiuv020@gmail.com>
2024-10-26 23:03:27 +0800

    create module example ios app

---
+++ packages/meeting_module/example/ios/meetingmoduleexample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  meetingmoduleexample
+//
+//  Created by aoeiuv on 2024/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ packages/meeting_module/example/ios/meetingmoduleexample/meetingmoduleexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  meetingmoduleexampleApp.swift
+//  meetingmoduleexample
+//
+//  Created by aoeiuv on 2024/10/26.
+//
+
+import SwiftUI
+
+@main
+struct meetingmoduleexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
