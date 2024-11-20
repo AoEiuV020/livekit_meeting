9f2dfe9248ce554d77afb83ea50efcf14ddc63b3
AoEiuV020 <aoeiuv020@gmail.com>
2024-11-21 00:02:27 +0800

    自动更新代码
    应该是分支切换导致插件变化之前误提交了个自动生成其他分支插件注册代码，

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,7 +13,6 @@ import livekit_client
 import meeting_flutter
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
@@ -24,5 +23,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MeetingFlutterPlugin.register(with: registry.registrar(forPlugin: "MeetingFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
