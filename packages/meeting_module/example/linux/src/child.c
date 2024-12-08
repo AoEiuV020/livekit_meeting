4a649eb2bdcd275387c5a4c8dff9f997b80400a5
AoEiuV020 <aoeiuv020@gmail.com>
2024-12-09 00:13:44 +0800

    尝试gtk嵌入子窗口
    目前一嵌入立马就被移除了，

--- packages/meeting_module/example/linux/src/child.c
+++ packages/meeting_module/example/linux/src/child.c
@@ -1,30 +1,56 @@
+// child.c
 #include <gtk/gtk.h>
+#include <gtk/gtkx.h>
+#include <gdk/gdkx.h>
 
 static void
 activate(GtkApplication *app, gpointer user_data)
 {
-    GtkWidget *window;
+    GtkWidget *plug;
     GtkWidget *label;
+    char **argv = (char **)user_data;
 
-    window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Child Window");
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 100);
+    if (argv && argv[1])
+    {
+        Window socket_id = strtoul(argv[1], NULL, 10);
+        g_print("Trying to create plug with socket ID: %lu\n", socket_id);
+        plug = gtk_plug_new(socket_id);
+        if (gtk_plug_get_embedded(GTK_PLUG(plug)))
+        {
+            g_print("Plug successfully embedded into the socket.\n");
+        }
+        else
+        {
+            g_print("Failed to embed Plug into the socket.\n");
+        }
+    }
+    else
+    {
+        plug = gtk_application_window_new(app);
+        gtk_window_set_title(GTK_WINDOW(plug), "Child Window");
+        gtk_window_set_default_size(GTK_WINDOW(plug), 200, 100);
+    }
 
-    label = gtk_label_new("Hello!");
-    gtk_container_add(GTK_CONTAINER(window), label);
+    label = gtk_label_new("Child Window Content");
+    gtk_container_add(GTK_CONTAINER(plug), label);
 
-    gtk_widget_show_all(window);
+    gtk_widget_show_all(plug);
 }
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
+    g_print("Child window activated with argc: %d\n", argc);
+    if (argc > 1)
+    {
+        g_print("Got socket ID parameter: %s\n", argv[1]);
+    }
     GtkApplication *app;
     int status;
 
-    app = gtk_application_new("org.gtk.example.child", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
+    app = gtk_application_new("org.gtk.example.child", G_APPLICATION_NON_UNIQUE);
+    // 直接传递 argv 到 activate 回调
+    g_signal_connect(app, "activate", G_CALLBACK(activate), argv);
+    status = g_application_run(G_APPLICATION(app), 0, argv);
     g_object_unref(app);
 
     return status;
--- packages/meeting_module/example/linux/src/main.c
+++ packages/meeting_module/example/linux/src/main.c
@@ -1,36 +1,105 @@
+// main.c
 #include <gtk/gtk.h>
-#include <stdlib.h>
+#include <gtk/gtkx.h>
+#include <gdk/gdkx.h>
+
+static GtkWidget *socket = NULL;
+static GPid child_pid = 0;
 
 static void
-button_clicked(GtkWidget *widget, gpointer data)
+launch_child_window(GtkWidget *widget, gpointer data)
 {
-    system("./child_window");
+    GError *error = NULL;
+    char *argv[] = {"./child_window", NULL};
+    g_spawn_async(NULL, argv, NULL,
+                 G_SPAWN_SEARCH_PATH | G_SPAWN_DO_NOT_REAP_CHILD,
+                 NULL, NULL, &child_pid, &error);
+    
+    if (error) {
+        g_print("Error launching child: %s\n", error->message);
+        g_error_free(error);
+    }
+}
+
+static void
+embed_child_window(GtkWidget *widget, gpointer data)
+{
+    if (!socket) {
+        return;
+    }
+    Window socket_id = gtk_socket_get_id(GTK_SOCKET(socket));
+    char socket_id_str[32];
+    g_snprintf(socket_id_str, sizeof(socket_id_str), "%lu", socket_id);
+    g_print("Trying to embed with socket ID: %s\n", socket_id_str);
+    GError *error = NULL;
+    char *argv[] = {"./child_window", socket_id_str, NULL};
+    g_spawn_async(NULL, argv, NULL,
+                 G_SPAWN_SEARCH_PATH | G_SPAWN_DO_NOT_REAP_CHILD,
+                 NULL, NULL, &child_pid, &error);
+
+    if (error) {
+        g_print("Error embedding child: %s\n", error->message);
+        g_error_free(error);
+    }
+}
+
+static void
+socket_plugged(GtkSocket *socket, gpointer data)
+{
+    g_print("Plug added to socket\n");
+}
+
+static void
+socket_unplugged(GtkSocket *socket, gpointer data)
+{
+    g_print("Plug removed from socket\n");
 }
 
 static void
 activate(GtkApplication *app, gpointer user_data)
 {
     GtkWidget *window;
-    GtkWidget *button;
+    GtkWidget *vbox;
+    GtkWidget *hbox;
+    GtkWidget *launch_button;
+    GtkWidget *embed_button;
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Main Window");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
 
-    button = gtk_button_new_with_label("Open Child Window");
-    g_signal_connect(button, "clicked", G_CALLBACK(button_clicked), NULL);
-    gtk_container_add(GTK_CONTAINER(window), button);
+    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 5);
+
+    launch_button = gtk_button_new_with_label("Launch Child Window");
+    g_signal_connect(launch_button, "clicked", G_CALLBACK(launch_child_window), NULL);
+    gtk_box_pack_start(GTK_BOX(hbox), launch_button, TRUE, TRUE, 5);
+
+    embed_button = gtk_button_new_with_label("Embed Child Window");
+    g_signal_connect(embed_button, "clicked", G_CALLBACK(embed_child_window), NULL);
+    gtk_box_pack_start(GTK_BOX(hbox), embed_button, TRUE, TRUE, 5);
+
+    socket = gtk_socket_new();
+    gtk_widget_set_size_request(socket, 200, 200);
+    g_signal_connect(socket, "plug-added", G_CALLBACK(socket_plugged), NULL);
+    g_signal_connect(socket, "plug-removed", G_CALLBACK(socket_unplugged), NULL);
+    gtk_box_pack_start(GTK_BOX(vbox), socket, TRUE, TRUE, 0);
 
     gtk_widget_show_all(window);
 }
-
 int
 main(int argc, char **argv)
 {
     GtkApplication *app;
     int status;
 
-    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    // 使用 G_APPLICATION_HANDLES_COMMAND_LINE 标志
+    app = gtk_application_new("org.gtk.example.main",
+                            G_APPLICATION_NON_UNIQUE |
+                            G_APPLICATION_HANDLES_OPEN);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
